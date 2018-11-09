b98de1b023a6b0671e4ccc9462c95e11fd28403f
Alexander Thompson <legoboyjr@gmail.com>
2018-11-08 19:05:03 -0500

    changed app to functional component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,13 @@ import Header from './Header';
 import FullPageView from './layouts/FullPageView';
 
 
-class App extends Component {
-  render() {
-    return (
-      <FullPageView>
-        <Header />
-        <Main />
-      </FullPageView>
-    );
-  }
+function App(props) {
+  return (
+    <FullPageView>
+      <Header />
+      <Main />
+    </FullPageView>
+  );
 }
 
 export default App;
