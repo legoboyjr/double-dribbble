3eef253e0b952ebb63dbfad1bc7dc6718275efba
Alexander Thompson <legoboyjr@gmail.com>
2018-11-08 20:04:57 -0500

    added proptypes for full page view

--- src/layouts/FullPageView.js
+++ src/layouts/FullPageView.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function FullPageView(props) {
     return (
@@ -6,6 +7,10 @@ function FullPageView(props) {
           {props.children}
       </div>
     );
+  }
+
+  FullPageView.protoTypes = {
+    children: PropTypes.array.isRequired
   }
 
 export default FullPageView;
