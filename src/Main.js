3832009c25f90e17d389d16506026a6d6b47d6da
Alexander Thompson <legoboyjr@gmail.com>
2018-11-09 19:12:28 -0500

    fixed cards and added 5 more

--- src/Main.js
+++ src/Main.js
@@ -1,33 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from './Card';
 
 
-function Main(props) {
-    return (<main className="h-100 d-flex flex-column">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center align-items-center">
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <a href="#" className="nav-link text-dark font-weight-bold">
-                        Popular <i className="fa fa-angle-down"></i></a>
-                </li>
-                <li className=" nav-item">
-                    <a href="#" className="nav-link text-dark font-weight-bold">
-                        Shots<i className="fa fa-angle-down"></i></a>
-                </li>
-                <li className=" nav-item">
-                    <a href="#" className="nav-link text-dark font-weight-bold">
-                        Now <i className="fa fa-angle-down"></i></a>
-                </li>
-            </ul>
-        </nav>
-        <section className="project-grid flex-grow-1 p-4 d-flex flex-wrap justify-content-around">
-            <Card src="https://loremflickr.com/180/180" likes={2108} views={2108} comments={2108} />
-            <Card src="https://loremflickr.com/180/180/dog" likes={2108} views={2108} comments={2108} />
-            <Card src="https://loremflickr.com/180/180/bird" likes={2108} views={2108} comments={2108} />
-            <Card src="https://loremflickr.com/180/180/snake" likes={2108} views={2108} comments={2108} />
-            <Card src="https://loremflickr.com/180/180/truck" likes={642} views={397} comments={15} />
-        </section>
-    </main>)
+
+class Main extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            cardData: [
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+                {
+                    src: "https://loremflickr.com/180/180/",
+                    views: 1000,
+                    likes: 300,
+                    comments: 2
+                },
+            ]
+        };
+    }
+    render() {
+        const cardsToRender = this.state.cardData
+                            .map((datum, index) => <Card src={datum.src}
+                                likes={datum.likes}
+                                views={datum.views}
+                                comments={datum.comments}
+                                key={index} />);
+                                
+        return (<main className="h-100 d-flex flex-column">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center align-items-center">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link text-dark font-weight-bold">
+                            Popular <i className="fa fa-angle-down"></i></a>
+                    </li>
+                    <li className=" nav-item">
+                        <a href="#" className="nav-link text-dark font-weight-bold">
+                            Shots<i className="fa fa-angle-down"></i></a>
+                    </li>
+                    <li className=" nav-item">
+                        <a href="#" className="nav-link text-dark font-weight-bold">
+                            Now <i className="fa fa-angle-down"></i></a>
+                    </li>
+                </ul>
+            </nav>
+            <section className="project-grid flex-grow-1 p-4 d-flex flex-wrap justify-content-around">
+                {cardsToRender}
+            </section>
+        </main>
+        );
+    }
 }
 
 export default Main;
